0a1dc3c38626de0492eed9bfc06501b2993adbe6
Zhuocheng Mahjong Deploy <deploy@zhuocheng-mahjong.local>
2026-09-20 14:45:44 +0800

    Point mini program at new cloud hosting service

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -1,6 +1,6 @@
 // 微信云托管：小程序经 callContainer 内网调用，不需要配置业务域名或备案。
-const CLOUD_ENV_ID = 'prod-d1g3k4lm8f215cc54'
-const CLOUD_SERVICE = 'express-8fh7'
+const CLOUD_ENV_ID = 'prod-d7gf8lw3n60ac72fa'
+const CLOUD_SERVICE = 'express-k468'
 const USE_CLOUD_RUN = true
 
 // 仅用于本机 Node 服务联调。正式小程序不使用该地址。
